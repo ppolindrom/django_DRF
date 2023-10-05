231d9d9739b36c8944b6b50ad9f4725b982c170b
ppolindrom <ipollyce@mail.ru>
2023-10-05 16:17:22 +0300

    Домашнее задание 25.1

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,7 +8,6 @@ from rest_framework_simplejwt.views import (
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user_update'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
